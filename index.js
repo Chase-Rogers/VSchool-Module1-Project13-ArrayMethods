375d08ccc92a3fcb7413caccc42c32cb53c1da87
Chase <sde.chase.rogers@gmail.com>
2020-06-02 17:46:38 -0600

    Finish

---
+++ index.js
@@ -0,0 +1,41 @@
+
+var fruit = ["banana", "apple", "orange", "watermelon"];
+var vegetables = ["carrot", "tomato", "pepper", "lettuce"];
+
+// 1
+vegetables.pop();
+console.log("vegetables:", vegetables);
+
+// 2
+fruit.shift();
+console.log("fruit: ", fruit);
+
+// 3
+console.log(fruit.indexOf('orange'));
+
+// 4
+fruit.push(fruit.indexOf("orange"))
+console.log("fruit: ", fruit);
+
+// 5
+console.log(vegetables.length);      
+
+// 6
+vegetables.push(vegetables.length);
+console.log('vegetables:', vegetables);
+
+// 7
+const food = fruit.concat(vegetables);
+console.log(food);
+
+// 8
+food.splice(4, 2);
+console.log(food);
+
+// 9
+food.reverse();
+console.log(food);
+
+// 10
+const shoppingList = food.toString();
+console.log(shoppingList);
